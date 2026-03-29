b2a205beb0d80baa95baf3be43935348ecaca067
hrdpsingh <harshdeep.s.rakkar@gmail.com>
2026-03-29 12:12:23 +0530

    Created a simple window to display the hostname.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,44 @@
-use hostname::get;
+use iced::widget::{row, text};
+use iced::{Alignment, Application, Command, Element, Settings, Theme, executor};
 
-fn main() {
-    let hostname = get().expect("Failed to get hostname");
-    println!("Hostname: {}", hostname.to_string_lossy());
+fn main() -> iced::Result {
+    ByteScope::run(Settings::default())
+}
+
+struct ByteScope {
+    hostname: String,
+}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+impl Application for ByteScope {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Theme = Theme;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Message>) {
+        let hostname = match hostname::get() {
+            Ok(name) => name.to_string_lossy().to_string(),
+            Err(_) => "Unavailable".to_string(),
+        };
+
+        (Self { hostname }, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("ByteScope")
+    }
+
+    fn update(&mut self, _message: Message) -> Command<Message> {
+        Command::none()
+    }
+
+    fn view(&self) -> Element<'_, Message> {
+        row![text("Hostname: "), text(&self.hostname)]
+            .padding(20)
+            .align_items(Alignment::Center)
+            .into()
+    }
 }
